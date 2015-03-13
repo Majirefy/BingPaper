c62d639ae2f7846ea96fe117a7eefa6f489dac23
Peng Jingwen <pengsrc@icloud.com>
2015-03-13 16:52:49 +0800

    Initial Commit
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

---
+++ BingPaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BingPaper
+//
+//  Created by Peng Jingwen on 2015-03-13.
+//  Copyright (c) 2015 Peng Jingwen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
